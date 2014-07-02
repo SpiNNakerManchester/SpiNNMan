08d1888f018c7daeeaf51e4a1de22facc2686644
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2014-07-02 08:33:33 +0100

    Updated to refer to the destination cores and links rather than a bitmask

--- spinnman/model/multicast_route.py
+++ spinnman/model/multicast_route.py
@@ -2,15 +2,17 @@ class MulticastRoute(object):
     """ Represents a multicast route
     """
     
-    def __init__(self, key, mask, route):
+    def __init__(self, key, mask, processor_ids, link_ids):
         """
         
         :param key: The routing key
         :type key: int
         :param mask: The route key mask
         :type mask: int
-        :param route: The route, as a spinnaker bit mask
-        :type route: int
+        :param processor_ids: The destination processor ids
+        :type processor_ids: iterable of int
+        :param link_ids: The destination link ids
+        :type link_ids: iterable of int
         :raise None: No known exceptions are raised
         """
         pass
@@ -34,10 +36,19 @@ class MulticastRoute(object):
         pass
     
     @property
-    def route(self):
-        """ The route as a bitmask of |16 core bits|6 link bits|
+    def processor_ids(self):
+        """ The destination processor ids
         
-        :return: The route
-        :rtype: int
+        :return: An iterable of processor ids
+        :rtype: iterable of int
+        """
+        pass
+    
+    @property
+    def link_ids(self):
+        """ The destination link ids
+        
+        :return: An iterable of link ids
+        :rtype: iterable of int
         """
         pass
