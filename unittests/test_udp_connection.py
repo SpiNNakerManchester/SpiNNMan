341fea79cbb955e6dd1e6c9c98e9df6561d0c519
Petrut Antoniu Bogdan <petrutantoniu8@gmail.com>
2014-07-07 14:48:04 +0100

    Created incomplete skeleton for testing. Not enough modules implemented >> skipping

---
+++ unittests/test_udp_connection.py
@@ -0,0 +1,16 @@
+__author__ = 'Petrut'
+
+import unittest
+import spinnman.connections.udp_connection as udp_conn
+class TestUDPConnection(unittest.TestCase):
+    def test_setup_new_udp_connection(self):
+        #localhost = "127.0.0.1"
+        #localport = "54321"
+        #remotehost = "192.16.240.253"
+        #remoteport = "12345"
+        #connection = udp_conn.UDPConnection(localhost,localport, remotehost)
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
