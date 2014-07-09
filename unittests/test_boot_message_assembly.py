734349c0d6cfc386b3567128fade60288e51e85c
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2014-07-09 22:26:39 +0100

    Fixed for moved op code

--- unittests/test_boot_message_assembly.py
+++ unittests/test_boot_message_assembly.py
@@ -3,12 +3,13 @@ __author__ = 'Petrut'
 import unittest
 import spinnman.exceptions as exc
 import spinnman.messages.spinnaker_boot_message as boot_msg
+from spinnman.messages.spinnaker_boot_op_code import SpinnakerBootOpCode
 
 class TestSpiNNakerBootMessage(unittest.TestCase):
     def test_create_new_boot_message(self):
-        msg = boot_msg.SpinnakerBootMessage(boot_msg.OpCode.HELLO,0,0,0)
+        msg = boot_msg.SpinnakerBootMessage(SpinnakerBootOpCode.HELLO,0,0,0)
         self.assertEqual(msg.data, None)
-        self.assertEqual(msg.opcode,boot_msg.OpCode.HELLO)
+        self.assertEqual(msg.opcode,SpinnakerBootOpCode.HELLO)
         self.assertEqual(msg.operand_1,0)
         self.assertEqual(msg.operand_2,0)
         self.assertEqual(msg.operand_3,0)
@@ -16,7 +17,7 @@ class TestSpiNNakerBootMessage(unittest.TestCase):
     """
     def test_raise_exception(self):
         with self.assertRaises(exc.SpinnmanInvalidParameterException):
-            msg = boot_msg.SpinnakerBootMessage(boot_msg.OpCode.HELLO,None,None,None)
+            msg = boot_msg.SpinnakerBootMessage(SpinnakerBootOpCode.HELLO,None,None,None)
 
     def test_raise_exception_op_code(self):
         with self.assertRaises(exc.SpinnmanInvalidParameterException):
