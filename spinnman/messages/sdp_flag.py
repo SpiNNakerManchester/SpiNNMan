1ea95d132705873929aca8c4ebbd62d2827138e6
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2014-07-10 09:25:16 +0100

    Renamed

--- spinnman/messages/sdp_flag.py
+++ spinnman/messages/sdp_flag.py
@@ -1,8 +1,8 @@
 from enum import Enum
 
 
-class Flag(Enum):
-    """ Flag for the message
+class SDPFlag(Enum):
+    """ SDPFlag for the message
     """
     REPLY_NOT_EXPECTED = (0x07, "Indicates that a reply is not expected")
     REPLY_EXPECTED = (0x87, "Indicates that a reply is expected")
