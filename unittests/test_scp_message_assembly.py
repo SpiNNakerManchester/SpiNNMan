c053499fe435d3c7050abce8085dad7c45b874bc
Petrut Antoniu Bogdan <petrutantoniu8@gmail.com>
2014-07-10 10:07:08 +0100

    Updated tests to match splitting Flags and Commands into separate files

--- unittests/test_scp_message_assembly.py
+++ unittests/test_scp_message_assembly.py
@@ -4,17 +4,19 @@ import unittest
 import spinnman.messages.scp_message as scp_msg
 import spinnman.messages.sdp_message as sdp_msg
 import spinnman.exceptions as exc
+import spinnman.messages.sdp_flag as flags
+import spinnman.messages.scp_command as cmds
 
 class TestSCPMessageAssembly(unittest.TestCase):
     def test_create_new_scp_message(self):
-        msg = scp_msg.SCPMessage(sdp_msg.Flag.REPLY_NOT_EXPECTED, 0, 1, 0, 0, 0, 1, 0, 0, 0,
-                                 scp_msg.Command.CMD_APLX, 0, 1, 2, 3, bytearray(0))
+        msg = scp_msg.SCPMessage(flags.SDPFlag.REPLY_NOT_EXPECTED, 0, 1, 0, 0, 0, 1, 0, 0, 0,
+                                 cmds.SCPCommand.CMD_APLX, 0, 1, 2, 3, bytearray(0))
 
-        self.assertEqual(msg.flags,sdp_msg.Flag.REPLY_NOT_EXPECTED)
+        self.assertEqual(msg.flags,flags.SDPFlag.REPLY_NOT_EXPECTED)
         self.assertEqual(msg.argument_1,1)
         self.assertEqual(msg.argument_2,2)
         self.assertEqual(msg.argument_3,3)
-        self.assertEqual(msg.command,scp_msg.Command.CMD_APLX)
+        self.assertEqual(msg.command,cmds.SCPCommand.CMD_APLX)
         self.assertEqual(msg.tag,0)
         self.assertEqual(msg.destination_port,1)
         self.assertEqual(msg.source_port,1)
@@ -41,13 +43,13 @@ class TestSCPMessageAssembly(unittest.TestCase):
     """
     def test_throwing_of_exception_port(self):
         with self.assertRaises(exc.SpinnmanInvalidParameterException):
-             msg = scp_msg.SCPMessage(sdp_msg.Flag.REPLY_NOT_EXPECTED, 0, -1, 0, 0, 0, 1, 0, 0, 0,
-                                 scp_msg.Command.CMD_APLX, 0, 1, 2, 3, bytearray(0))
+             msg = scp_msg.SCPMessage(flags.SDPFlag.REPLY_NOT_EXPECTED, 0, -1, 0, 0, 0, 1, 0, 0, 0,
+                                 cmds.SCPCommand.CMD_APLX, 0, 1, 2, 3, bytearray(0))
 
     def test_throwing_of_exception_coordinates(self):
         with self.assertRaises(exc.SpinnmanInvalidParameterException):
-             msg = scp_msg.SCPMessage(sdp_msg.Flag.REPLY_NOT_EXPECTED, 0, 1, -1, 0, 0, 1, 0, 0, 0,
-                                 scp_msg.Command.CMD_APLX, 0, 1, 2, 3, bytearray(0))
+             msg = scp_msg.SCPMessage(flags.SDPFlag.REPLY_NOT_EXPECTED, 0, 1, -1, 0, 0, 1, 0, 0, 0,
+                                 cmds.SCPCommand.CMD_APLX, 0, 1, 2, 3, bytearray(0))
 
 if __name__ == '__main__':
     unittest.main()
--- unittests/test_sdp_message_assembly.py
+++ unittests/test_sdp_message_assembly.py
@@ -4,12 +4,14 @@ import unittest
 import spinnman.messages.scp_message as scp_msg
 import spinnman.messages.sdp_message as sdp_msg
 import spinnman.exceptions as exc
+import spinnman.messages.sdp_flag as flags
+import spinnman.messages.scp_command as cmds
 
 class TestSDPMessageAssembly(unittest.TestCase):
     def test_create_new_sdp_message(self):
-        msg = sdp_msg.SDPMessage(sdp_msg.Flag.REPLY_NOT_EXPECTED,0,1,0,0,0,1,0,0,0,bytearray(0))
+        msg = sdp_msg.SDPMessage(flags.SDPFlag.REPLY_NOT_EXPECTED,0,1,0,0,0,1,0,0,0,bytearray(0))
 
-        self.assertEqual(msg.flags,sdp_msg.Flag.REPLY_NOT_EXPECTED)
+        self.assertEqual(msg.flags,flags.SDPFlag.REPLY_NOT_EXPECTED)
         self.assertEqual(msg.tag,0)
         self.assertEqual(msg.destination_port,1)
         self.assertEqual(msg.source_port,1)
@@ -27,19 +29,19 @@ class TestSDPMessageAssembly(unittest.TestCase):
 
     def test_throwing_of_exception_command(self):
         with self.assertRaises(exc.SpinnmanInvalidParameterException):
-            msg = scp_msg.SCPMessage(sdp_msg.Flag.REPLY_NOT_EXPECTED,0,1,0,0,0,1,0,0,0,10,0,1,2,3,bytearray(0))
+            msg = scp_msg.SCPMessage(flags.SDPFlag.REPLY_NOT_EXPECTED,0,1,0,0,0,1,0,0,0,10,0,1,2,3,bytearray(0))
 
     def test_throwing_of_exception_data(self):
         with self.assertRaises(exc.SpinnmanInvalidParameterException):
-            msg = scp_msg.SCPMessage(sdp_msg.Flag.REPLY_NOT_EXPECTED,0,1,0,0,0,1,0,0,0,scp_msg.Command.CMD_APLX,0,1,2,3,0)
+            msg = scp_msg.SCPMessage(flags.SDPFlag.REPLY_NOT_EXPECTED,0,1,0,0,0,1,0,0,0,scp_msg.Command.CMD_APLX,0,1,2,3,0)
     """
 
     def test_throwing_of_exception_port(self):
         with self.assertRaises(exc.SpinnmanInvalidParameterException):
-             msg = sdp_msg.SDPMessage(sdp_msg.Flag.REPLY_NOT_EXPECTED, 0, -1, 0, 0, 0, 1, 0, 0, 0,bytearray(0))
+             msg = sdp_msg.SDPMessage(flags.SDPFlag.REPLY_NOT_EXPECTED, 0, -1, 0, 0, 0, 1, 0, 0, 0,bytearray(0))
 
     def test_throwing_of_exception_coordinates(self):
         with self.assertRaises(exc.SpinnmanInvalidParameterException):
-             msg = sdp_msg.SDPMessage(sdp_msg.Flag.REPLY_NOT_EXPECTED, 0, 1, -1, 0, 0, 1, 0, 0, 0,bytearray(0))
+             msg = sdp_msg.SDPMessage(flags.SDPFlag.REPLY_NOT_EXPECTED, 0, 1, -1, 0, 0, 1, 0, 0, 0,bytearray(0))
 if __name__ == '__main__':
     unittest.main()
