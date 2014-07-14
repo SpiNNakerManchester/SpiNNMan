db5c4755514e9daa971525b8760b691e8e4fef76
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2014-07-14 13:05:43 +0100

    Added all the submodules

--- setup.py
+++ setup.py
@@ -8,6 +8,13 @@ setup(
     version="0.1-SNAPSHOT",
     description="Interaction with a SpiNNaker Machine",
     url="https://github.com/SpiNNakerManchester/SpiNNMan",
-    packages=['spinnman'],
+    packages=['spinnman',
+            'spinnman.connections',
+            'spinnman.data',
+            'spinnman.messages',
+            'spinnman.messages.scp',
+            'spinnman.messages.scp.impl',
+            'spinnman.messages.sdp'
+            'spinnman.model'],
     install_requires=['six', 'enum34', 'SpiNNMachine'],
 )
