16289192d52d302b3c353ac9eac148ad3f1b5cbd
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2014-07-14 14:56:25 +0100

    Updated requirements

--- setup.py
+++ setup.py
@@ -15,5 +15,6 @@ setup(
             'spinnman.messages.scp',
             'spinnman.messages.scp.impl',
             'spinnman.messages.sdp',
-            'spinnman.model']
+            'spinnman.model'],
+    install_requires=['six', 'enum34', 'SpiNNMachine']
 )
