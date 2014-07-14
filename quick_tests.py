e143f2ce7f0256579ef2b12cde6d775d0bb7c774
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2014-07-14 14:49:37 +0100

    Removed my test code

--- quick_tests.py
+++
@@ -1,9 +0,0 @@
-from spinnman.connections.udp_connection import UDPConnection
-from spinnman.messages.scp.impl.scp_read_memory_request import SCPReadMemoryRequest
-from spinnman.messages.scp.impl.scp_read_memory_response import SCPReadMemoryResponse
-
-connection = UDPConnection(remote_host="spinn-10.cs.man.ac.uk")
-request = SCPReadMemoryRequest(0, 0, 0x70000000, 256)
-connection.send_scp_request(request)
-response = SCPReadMemoryResponse()
-connection.receive_scp_response(response)
