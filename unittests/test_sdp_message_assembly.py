16dd1618ab4bcdafdc90eca72304f081c205b091
Petrut Antoniu Bogdan <petrutantoniu8@gmail.com>
2014-07-15 10:46:01 +0100

    Updated SDP message assembly to work with the new message modules

--- unittests/test_sdp_message_assembly.py
+++ unittests/test_sdp_message_assembly.py
@@ -1,34 +1,36 @@
 __author__ = 'Petrut'
 
 import unittest
-import spinnman.messages.scp_message as scp_msg
-import spinnman.messages.sdp_message as sdp_msg
+import spinnman.messages.sdp.sdp_message as sdp_msg
 import spinnman.exceptions as exc
-import spinnman.messages.sdp_flag as flags
-import spinnman.messages.scp_command as cmds
+import spinnman.messages.sdp.sdp_flag as flags
+import spinnman.messages.sdp.sdp_header as sdp_header
 
 class TestSDPMessageAssembly(unittest.TestCase):
     def test_create_new_sdp_message(self):
-        msg = sdp_msg.SDPMessage(flags.SDPFlag.REPLY_NOT_EXPECTED,1,0,0,0,0,1,0,0,0,bytearray(0))
+        sdp_h = sdp_header.SDPHeader(flags.SDPFlag.REPLY_NOT_EXPECTED,0,1,0,0,0,1,0,0,0)
+        msg = sdp_msg.SDPMessage(sdp_h,bytearray(0))
 
-        self.assertEqual(msg.flags,flags.SDPFlag.REPLY_NOT_EXPECTED)
-        self.assertEqual(msg.tag,0)
-        self.assertEqual(msg.destination_port,1)
-        self.assertEqual(msg.source_port,1)
-        self.assertEqual(msg.destination_chip_x,0)
-        self.assertEqual(msg.destination_chip_y,0)
-        self.assertEqual(msg.source_chip_x,0)
-        self.assertEqual(msg.source_chip_y,0)
-        self.assertEqual(msg.destination_cpu,0)
-        self.assertEqual(msg.source_cpu,0)
+        self.assertEqual(msg.sdp_header.flags,flags.SDPFlag.REPLY_NOT_EXPECTED)
+        self.assertEqual(msg.sdp_header.tag,0)
+        self.assertEqual(msg.sdp_header.destination_port,1)
+        self.assertEqual(msg.sdp_header.source_port,1)
+        self.assertEqual(msg.sdp_header.destination_chip_x,0)
+        self.assertEqual(msg.sdp_header.destination_chip_y,0)
+        self.assertEqual(msg.sdp_header.source_chip_x,0)
+        self.assertEqual(msg.sdp_header.source_chip_y,0)
+        self.assertEqual(msg.sdp_header.destination_cpu,0)
+        self.assertEqual(msg.sdp_header.source_cpu,0)
         self.assertEqual(msg.data,bytearray(0))
 
     def test_throwing_of_exception_port(self):
         with self.assertRaises(exc.SpinnmanInvalidParameterException):
-             sdp_msg.SDPMessage(flags.SDPFlag.REPLY_NOT_EXPECTED,-1,0,0,0,0,1,0,0,0,bytearray(0))
+            sdp_header.SDPHeader(flags.SDPFlag.REPLY_NOT_EXPECTED,-1,1,0,0,0,1,0,0,0)
 
     def test_throwing_of_exception_coordinates(self):
         with self.assertRaises(exc.SpinnmanInvalidParameterException):
-             sdp_msg.SDPMessage(flags.SDPFlag.REPLY_NOT_EXPECTED,1,0,-1,0,0,1,0,0,0,bytearray(0))
+            sdp_header.SDPHeader(flags.SDPFlag.REPLY_NOT_EXPECTED,0,1,-1,0,0,1,0,0,0)
+
+
 if __name__ == '__main__':
     unittest.main()
