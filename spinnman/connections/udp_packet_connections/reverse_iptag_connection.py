d4e07c949a6b2371d2cc426b706e97ed2eda20c9
daviess <sergio.davies@gmail.com>
2014-10-17 16:59:16 +0100

    corrected bugs here and there

--- spinnman/connections/udp_packet_connections/reverse_iptag_connection.py
+++ spinnman/connections/udp_packet_connections/reverse_iptag_connection.py
@@ -13,7 +13,7 @@ class ReverseIPTagConnection(AbstractUDPConnection, AbstractUDPEIEIOReceiver,
                              AbstractUDPEIEIOSender):
 
     def __init__(self, local_host=None, local_port=None, remote_host=None,
-                 remote_port=constants.UDP_BOOT_CONNECTION_DEFAULT_PORT):
+                 remote_port=None):
         AbstractUDPConnection.__init__(self, local_host, local_port,
                                        remote_host, remote_port)
 
