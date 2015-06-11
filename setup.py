e0ca3e2d1458ea9b61a03a132bcf3e4544ee6cf7
Alan Stokes <alan.barry.stokes@gmail.com>
2015-06-11 14:24:35 +0100

    Update setup.py
    grrg

--- setup.py
+++ setup.py
@@ -36,7 +36,8 @@ setup(
               'spinnman.messages.spinnaker_boot._system_variables',
               'spinnman.messages.udp_utils',
               'spinnman.model',
-              'spinnman.re_injection_binary'],
+              'spinnman.re_injection_binary',
+              'spinnman.re_injection_binary.re_injection'],
     package_data={'spinnman.messages.spinnaker_boot': ['boot_data/*.boot']},
     install_requires=['six', 'enum34', 'SpiNNMachine >= 2015.003-rc-01']
 )
