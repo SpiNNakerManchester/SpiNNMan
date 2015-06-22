57a03f696975bd2f15e977d021addd2f4087a0de
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2015-06-22 15:21:25 +0100

    Fixed an issue with tuples

--- spinnman/messages/scp/abstract_messages/abstract_scp_bmp_request.py
+++ spinnman/messages/scp/abstract_messages/abstract_scp_bmp_request.py
@@ -29,7 +29,7 @@ class AbstractSCPBMPRequest(AbstractSCPRequest):
         sdp_header = SDPHeader(
             flags=SDPFlag.REPLY_EXPECTED, destination_port=0,
             destination_cpu=AbstractSCPBMPRequest.get_first_board(boards),
-            destination_chip_x=0, destination_chip_y=0),
+            destination_chip_x=0, destination_chip_y=0)
         AbstractSCPRequest.__init__(self, sdp_header, scp_request_header,
                                     argument_1, argument_2, argument_3, data)
 
