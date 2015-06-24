4c56f42df4e1d4146db1ce4d936ba0c54807607a
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2015-06-24 20:09:48 +0100

    Closer to working

--- spinnman/processes/read_iobuf_process.py
+++ spinnman/processes/read_iobuf_process.py
@@ -1,4 +1,7 @@
 import functools
+import struct
+from collections import defaultdict
+
 from spinnman.processes.get_cpu_info_process import GetCPUInfoProcess
 from spinnman.model.io_buffer import IOBuffer
 from spinnman.messages.scp.impl.scp_read_memory_request \
@@ -17,23 +20,32 @@ class ReadIOBufProcess(AbstractMultiConnectionProcess):
         self._machine = machine
 
         # A dictionary of (x, y, p) -> str
-        self._iobuf = dict()
+        self._iobuf = defaultdict("")
 
-    def handle_first_iobuf_response(self, x, y, p, response):
-        pass
+    def handle_iobuf_response(self, x, y, p, base_address, response):
+        (next_address, bytes_to_read) = struct.unpack_from(
+            "<I8xI", response.data, response.offset)
+        packet_bytes = response.length - 16
+        if bytes_to_read < response.length - 16:
+            offset = response.offset + 16
+            self._iobuf[(x, y, p)] += response.data[
+                offset:(offset + packet_bytes)]
+        bytes_to_read -= packet_bytes
 
-    def handle_remaining_iobuf_response(self, x, y, p, response):
-        pass
 
     def read_iobuf(self, chip_info, core_subsets):
         cpu_info_process = GetCPUInfoProcess(self._machine, self._connections)
         cpu_information = cpu_info_process.get_cpu_info(chip_info,
                                                         core_subsets)
 
+        # Kick-start the reading of the IOBufs
         for cpu_info in cpu_information:
-            this_chip_info = chip_info[(cpu_info.x, cpu_info.y)]
-            iobuf_bytes = this_chip_info.iobuf_size
-            base_address = cpu_info.iobuf_address
+            self._send_request(
+                SCPReadMemoryRequest(cpu_info.x, cpu_info.y,
+                                     cpu_info.iobuf_address, 256),
+                functools.partial(self.handle_iobuf_response,
+                                  cpu_info.x, cpu_info.y, cpu_info.p,
+                                  cpu_info.iobuf_address)
 
         self._finish()
         self.check_for_error()
