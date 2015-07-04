866edef0bcf8e08ab87cdde31f4111fd901e8466
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2015-07-04 21:08:05 +0100

    Added in the corrections for the SDRAM fixes

--- spinnman/processes/get_machine_process.py
+++ spinnman/processes/get_machine_process.py
@@ -99,7 +99,9 @@ class GetMachineProcess(AbstractMultiConnectionProcess):
         # Create the chip
         chip = Chip(
             x=chip_details.x, y=chip_details.y, processors=processors,
-            router=router, sdram=SDRAM(SDRAM.DEFAULT_SDRAM_BYTES),
+            router=router, sdram=SDRAM(
+                user_base_address=chip_details.sdram_base_address,
+                system_base_address=chip_details.system_sdram_base_address),
             ip_address=chip_details.ip_address,
             nearest_ethernet_x=chip_details.nearest_ethernet_x,
             nearest_ethernet_y=chip_details.nearest_ethernet_y)
