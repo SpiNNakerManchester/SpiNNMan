6bbb4793efea78965d306576f60f660bb56f7cc6
alan-stokes <alan.barry.stokes@gmail.com>
2015-08-28 14:54:52 +0100

    added the ability to unpack a eieio data message with a  prefix

--- spinnman/messages/eieio/data_messages/eieio_data_header.py
+++ spinnman/messages/eieio/data_messages/eieio_data_header.py
@@ -216,6 +216,13 @@ class EIEIODataHeader(object):
                 else:
                     payload_prefix = struct.unpack_from(
                         "<I", data, offset + 2)[0]
+        else:
+            if prefix_flag == 1:
+                if (eieio_type == EIEIOType.KEY_16_BIT or
+                        eieio_type == EIEIOType.KEY_PAYLOAD_16_BIT):
+                    prefix = struct.unpack_from("<H", data, offset + 2)[0]
+                else:
+                    prefix = struct.unpack_from("<I", data, offset + 2)[0]
 
         return EIEIODataHeader(
             eieio_type=eieio_type, tag=tag, prefix=prefix,
