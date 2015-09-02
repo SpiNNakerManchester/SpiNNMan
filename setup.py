71b8a02d9673db7375d1e1bb11fdef1e060924de
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2015-09-02 08:04:18 +0100

    Updated version for release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 
 setup(
     name="SpiNNMan",
-    version="2015.004-rc-01",
+    version="2015.004",
     description="Interaction with a SpiNNaker Machine",
     url="https://github.com/SpiNNakerManchester/SpiNNMan",
     license="GNU GPLv3.0",
@@ -36,5 +36,5 @@ setup(
               'spinnman.utilities'],
     package_data={'spinnman.messages.spinnaker_boot': ['boot_data/*.boot'],
                   'spinnman.model_binaries': ['*.aplx']},
-    install_requires=['six', 'enum34', 'SpiNNMachine == 2015.004-rc-01']
+    install_requires=['six', 'enum34', 'SpiNNMachine == 2015.004']
 )
