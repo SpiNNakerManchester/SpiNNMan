4d682416a94673dcd5bd8b878f734556847a225b
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2016-10-13 11:28:42 +0100

    Added an interface for access to SDRAM with a file-like interface

---
+++ integration_tests/test_memory_io.py
@@ -0,0 +1,29 @@
+import unittest
+from spinnman.transceiver import create_transceiver_from_hostname
+from spinnman.utilities.memory_io import MemoryIO
+import numpy
+
+
+class TestMemoryIO(unittest.TestCase):
+
+    def test_memory_io(self):
+        n_bytes = 10000
+        read_start_offset = 5
+        read_end_offset = -10
+        transceiver = create_transceiver_from_hostname(
+            "spinn-10.cs.man.ac.uk", 2)
+        transceiver.ensure_board_is_ready()
+        address = transceiver.malloc_sdram(0, 0, n_bytes, 30)
+        data = bytearray(numpy.random.randint(0, 256, n_bytes).astype("uint8"))
+        memory = MemoryIO(transceiver, 0, 0, address, address + n_bytes)
+        print "Written", memory.write(bytes(data)), "bytes"
+        memory = memory[read_start_offset:read_end_offset]
+        read_data = bytearray(memory.read())
+        compare_data = data[read_start_offset:read_end_offset]
+        print "Read", len(read_data), "bytes"
+        data_str = [hex(val) for val in compare_data]
+        read_str = [hex(val) for val in read_data]
+        self.assertEqual(
+            compare_data, read_data,
+            "Written data is not the same as read data:\n    {}\n    {}"
+            .format(data_str, read_str))
---
+++ spinnman/utilities/memory_io.py
@@ -0,0 +1,257 @@
+import os
+
+
+class MemoryIO(object):
+    """ A file-like object for reading and writing memory
+    """
+
+    __slots__ = [
+
+        # The transceiver for speaking to the machine
+        "_transceiver",
+
+        # The x coordinate of the chip to communicate with
+        "_x",
+
+        # The y coordinate of the chip to communicate with
+        "_y",
+
+        # The start address of the region to write to
+        "_start_address",
+
+        # The current pointer where read and writes are taking place
+        "_current_address",
+
+        # The current pointer where the next buffered write will occur
+        "_write_address",
+
+        # The end of the region to write to
+        "_end_address",
+
+        # The write buffer size
+        "_buffer_size",
+
+        # The write buffer bytearray
+        "_write_buffer",
+
+        # The write buffer memory view
+        "_write_memory_view",
+
+        # The write buffer pointer
+        "_write_buffer_offset"
+    ]
+
+    def __init__(
+            self, transceiver, x, y, start_address, end_address,
+            buffer_size=256):
+        """
+
+        :param transceiver: The transceiver to read and write with
+        :param x: The x-coordinate of the chip to write to
+        :param y: The y-coordinate of the chip to write to
+        :param start_address: The start address of the region to write to
+        :param end_address:\
+            The end address of the region to write to.  This is the first\
+            address just outside the region
+        :param buffer_size:\
+            The size of the write buffer to improve efficiency.  This will be\
+            truncated to the size of the region
+        """
+        if start_address >= end_address:
+            raise ValueError("Start address must be less than end address")
+
+        self._transceiver = transceiver
+        self._x = x
+        self._y = y
+        self._start_address = start_address
+        self._current_address = start_address
+        self._write_address = start_address
+        self._end_address = end_address
+        self._buffer_size = buffer_size
+        if buffer_size > (end_address - start_address):
+            self._buffer_size = end_address - start_address
+        self._write_buffer = bytearray(self._buffer_size)
+        self._write_memory_view = memoryview(self._write_buffer)
+        self._write_buffer_offset = 0
+
+    def __len__(self):
+        """ The size of the entire region of memory
+        """
+        return self._end_address - self._start_address
+
+    def __getitem__(self, new_slice):
+        """ Get a sub-region of this memory object.  The index or slice must\
+            be in range of the current region to be valid.
+
+        :param new_slice:\
+            A single index for a single byte of memory, or a contiguous slice
+        :rtype: :py:class:`~MemoryIO`
+        :raise ValueError:\
+            If the index or slice is outside of the current region
+        """
+        if isinstance(new_slice, int):
+            if new_slice >= len(self):
+                raise ValueError("Index {} out of range".format)
+            return MemoryIO(
+                self._transceiver, self._x, self._y,
+                self._start_address + new_slice,
+                self._start_address + new_slice + 1,
+                buffer_size=self._buffer_size)
+        elif isinstance(new_slice, slice):
+            if new_slice.step is not None and new_slice.step != 1:
+                raise ValueError("Slice must be contiguous")
+            start_address = self._start_address
+            end_address = self._end_address
+            if new_slice.start is not None:
+                if new_slice.start < 0:
+                    start_address = self._end_address + new_slice.start
+                else:
+                    start_address = self._start_address + new_slice.start
+            if new_slice.stop is not None:
+                if new_slice.stop > 0:
+                    end_address = self._start_address + new_slice.stop
+                else:
+                    end_address = self._end_address + new_slice.stop
+
+            if (start_address < self._start_address or
+                    end_address < self._start_address or
+                    end_address > self._end_address or
+                    start_address > self._end_address):
+                raise ValueError("Slice {} outside of this region".format(
+                    new_slice))
+            if start_address == end_address:
+                raise ValueError("Zero sized regions are not supported")
+
+            return MemoryIO(
+                self._transceiver, self._x, self._y,
+                start_address, end_address, buffer_size=self._buffer_size)
+
+    def __enter__(self):
+        """ Enter a new block which will call :py:meth:`~.close` when exited.
+        """
+        return self
+
+    def __exit__(self, exception_type, exception_value, traceback):
+        """ Exit a block and call :py:meth:`~.close`.
+        """
+        self.close()
+
+    def _flush_write_buffer(self):
+        """ Force the writing of the current write buffer
+        """
+        if self._write_buffer_offset > 0:
+            self._transceiver.write_memory(
+                self._x, self._y, self._write_address, self._write_buffer,
+                n_bytes=self._write_buffer_offset)
+            self._write_address += self._write_buffer_offset
+            self._write_buffer_offset = 0
+
+    def close(self):
+        """ Close the IO object
+        """
+        self._flush_write_buffer()
+
+    @property
+    def closed(self):
+        return False
+
+    def flush(self):
+        """ Flush any outstanding written data
+        """
+        self._flush_write_buffer()
+
+    def seek(self, n_bytes, from_what=os.SEEK_SET):
+        """ Seek to a position within the region
+        """
+        position = 0
+        if from_what == os.SEEK_SET:
+            position = self._start_address + n_bytes
+        elif from_what == os.SEEK_CUR:
+            position = self._current_address + n_bytes
+        elif from_what == os.SEEK_END:
+            position = self._end_address + n_bytes
+        else:
+            raise ValueError(
+                "Value of from_what must be one of os.SEEK_SET, os.SEEK_CUR"
+                " or os.SEEK_END")
+
+        if position < self._start_address or position > self._end_address:
+            raise ValueError(
+                "Attempt to seek to a position of {} which is outside of the"
+                " region".format(position))
+
+        self._flush_write_buffer()
+        self._current_address = position
+        self._write_address = position
+
+    def tell(self):
+        """ Return the current position within the region relative to the start
+        """
+        return self._current_address - self._start_address
+
+    @property
+    def address(self):
+        """ Return the current absolute address within the region
+        """
+        return self._current_address
+
+    def read(self, n_bytes=None):
+        """ Read a number of bytes, or the rest of the data if n_bytes is None\
+            or negative
+
+        :param n_bytes: The number of bytes to read
+        :rtype: bytes
+        :raise EOFError: If the read will be beyond the end of the region
+        """
+        if n_bytes == 0:
+            return b""
+
+        bytes_to_read = n_bytes
+        if n_bytes is None or n_bytes < 0:
+            bytes_to_read = self._end_address - self._current_address
+
+        if self._current_address + bytes_to_read > self._end_address:
+            raise EOFError
+
+        self._flush_write_buffer()
+        data = self._transceiver.read_memory(
+            self._x, self._y, self._current_address, bytes_to_read)
+        self._current_address += bytes_to_read
+        self._write_address = self._current_address
+
+        return data
+
+    def write(self, data):
+        """ Write some data to the region
+
+        :param data: The data to write
+        :type data: bytes
+        :return: The number of bytes written
+        :rtype: int
+        :raise EOFError: If the write will go over the end of the region
+        """
+        n_bytes = len(data)
+        if self._current_address + n_bytes > self._end_address:
+            raise EOFError
+
+        if n_bytes >= self._buffer_size:
+            self._flush_write_buffer()
+            self._transceiver.write_memory(
+                self._x, self._y, self._current_address, data)
+            self._current_address += n_bytes
+            self._write_address = self._current_address
+
+        else:
+            n_bytes_to_copy = min(
+                n_bytes, self._buffer_size - self._write_buffer_offset)
+            self._write_memory_view[
+                self._write_buffer_offset:
+                self._write_buffer_offset + n_bytes_to_copy
+            ] = data[:n_bytes_to_copy]
+            n_bytes -= n_bytes_to_copy
+            if self._write_buffer_offset == self._buffer_size:
+                self._flush_write_buffer()
+            if n_bytes > 0:
+                self._write_memory_view[:n_bytes] = data[n_bytes_to_copy:]
+
+        return n_bytes
