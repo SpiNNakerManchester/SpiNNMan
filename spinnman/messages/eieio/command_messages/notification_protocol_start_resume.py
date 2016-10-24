c5faf1f0654475c284c55d773822e104c82101a9
alan-stokes <alan.barry.stokes@gmail.com>
2016-10-24 11:50:53 +0100

    work that fixes the notification protocol to support both start/resume and pause/stop functionality.
    Also includes the removal of the assumption of mapping between keys and neurons.
    
     Next step is to finish the push bot trnaslation of retina packets from ethernet

---
+++ spinnman/messages/eieio/command_messages/notification_protocol_start_resume.py
@@ -0,0 +1,26 @@
+
+from spinnman.messages.eieio.command_messages.eieio_command_message\
+    import EIEIOCommandMessage
+from spinnman.messages.eieio.command_messages.eieio_command_header\
+    import EIEIOCommandHeader
+from spinnman import constants
+
+
+class NotificationProtocolStartResume(EIEIOCommandMessage):
+    """ Packet which contains the path to the database created by the\
+        toolchain which is to be used by any software which interfaces with\
+        SpiNNaker
+    """
+    def __init__(self):
+        EIEIOCommandMessage.__init__(
+            self, EIEIOCommandHeader(
+                constants.EIEIO_COMMAND_IDS.START_RESUME_NOTIFICATION.value))
+
+    @property
+    def bytestring(self):
+        data = super(NotificationProtocolStartResume, self).bytestring
+        return data
+
+    @staticmethod
+    def from_bytestring(command_header, data, offset):
+        return NotificationProtocolStartResume()
