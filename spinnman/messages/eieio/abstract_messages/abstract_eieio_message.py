9756963aab84c4d6748fa7148cc6a06b3ad5e42d
Christian <brenninc@cs.man.ac.uk>
2017-03-16 10:04:43 +0000

    remove AbstractBase from class with NO abstract Methods

--- spinnman/messages/eieio/abstract_messages/abstract_eieio_message.py
+++ spinnman/messages/eieio/abstract_messages/abstract_eieio_message.py
@@ -1,8 +1,3 @@
-from six import add_metaclass
-
-from spinn_utilities.abstract_base import AbstractBase
-
-@add_metaclass(AbstractBase)
 class AbstractEIEIOMessage(object):
     """ Marker interface for an EIEIOMessage
     """
