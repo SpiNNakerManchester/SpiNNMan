c57a403d8e3edcb8bd6f449abc1f10d1fefc6d9a
mbassor2 <mbassor2@cspc457.cs.man.ac.uk>
2017-04-06 08:55:59 +0100

    Tidied changes and made comments flake8 compliant

--- spinnman/transceiver.py
+++ spinnman/transceiver.py
@@ -2855,24 +2855,18 @@ class Transceiver(object):
                     (connection, listener) = (a_connection, a_listener)
                     break
 
-        ########## O Rhodes Edit ############
         # Create a connection if there isn't already one, and a listener
         if connection is None:
             connection = connection_class(local_host=local_host)
             self._all_connections.add(connection)
-        #listener = ConnectionListener(connection)
-        #listener.start()
-        #self._udp_receive_connections_by_port[connection.local_port][
-        #    local_host] = (connection, listener)
-        #connections_of_class.append((connection, listener))
-        #listener.add_callback(callback)
+            # indented from
             listener = ConnectionListener(connection)
             listener.start()
             self._udp_receive_connections_by_port[connection.local_port][
                 local_host] = (connection, listener)
             connections_of_class.append((connection, listener))
             listener.add_callback(callback)
-
+            # to here
         return connection
 
     def enable_reinjection(self, multicast=True, point_to_point=False,
