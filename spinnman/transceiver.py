e42c62829965268cd92e25d76d01899455c67544
mbassor2 <mbassor2@cspc457.cs.man.ac.uk>
2017-05-04 15:00:30 +0100

    Testing unindenting

--- spinnman/transceiver.py
+++ spinnman/transceiver.py
@@ -2837,7 +2837,7 @@ class Transceiver(object):
                 listener.start()
                 receiving_connections[local_host] = (connection, listener)
                 connections_of_class.append((connection, listener))
-            listener.add_callback(callback)
+                listener.add_callback(callback)
             return connection
 
         # If we are here, the local port wasn't specified to try to use an
@@ -2859,13 +2859,13 @@ class Transceiver(object):
         if connection is None:
             connection = connection_class(local_host=local_host)
             self._all_connections.add(connection)
-            listener = ConnectionListener(connection)
-            listener.start()
-            self._udp_receive_connections_by_port[connection.local_port][
+        listener = ConnectionListener(connection)
+        listener.start()
+        self._udp_receive_connections_by_port[connection.local_port][
                 local_host] = (connection, listener)
-            connections_of_class.append((connection, listener))
-            listener.add_callback(callback)
-            return connection
+        connections_of_class.append((connection, listener))
+        listener.add_callback(callback)
+        return connection
 
     def enable_reinjection(self, multicast=True, point_to_point=False,
                            nearest_neighbour=False, fixed_route=False):
