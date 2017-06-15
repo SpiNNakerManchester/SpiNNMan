3faec28c894d61df913c990bb5be890cc219c41f
Donal Fellows <donal.k.fellows@manchester.ac.uk>
2017-06-15 11:37:01 +0100

    More general slaughtering of verbosity

--- spinnman/messages/eieio/create_eieio_command.py
+++ spinnman/messages/eieio/create_eieio_command.py
@@ -1,26 +1,15 @@
-from spinnman.messages.eieio.command_messages.padding_request\
-    import PaddingRequest
-from spinnman.messages.eieio.command_messages.event_stop_request\
-    import EventStopRequest
-from spinnman.messages.eieio.command_messages.stop_requests\
-    import StopRequests
-from spinnman.messages.eieio.command_messages.start_requests\
-    import StartRequests
-from spinnman.messages.eieio.command_messages.spinnaker_request_buffers\
-    import SpinnakerRequestBuffers
-from spinnman.messages.eieio.command_messages.host_send_sequenced_data\
-    import HostSendSequencedData
-from spinnman.messages.eieio.command_messages.spinnaker_request_read_data\
-    import SpinnakerRequestReadData
-from spinnman.messages.eieio.command_messages.host_data_read\
-    import HostDataRead
-from spinnman.messages.eieio.command_messages.eieio_command_header\
-    import EIEIOCommandHeader
-from spinnman import constants
-from spinnman.messages.eieio.command_messages.eieio_command_message\
-    import EIEIOCommandMessage
-from spinnman.messages.eieio.command_messages.database_confirmation\
-    import DatabaseConfirmation
+from spinnman.messages.eieio.command_messages import PaddingRequest
+from spinnman.messages.eieio.command_messages import EventStopRequest
+from spinnman.messages.eieio.command_messages import StopRequests
+from spinnman.messages.eieio.command_messages import StartRequests
+from spinnman.messages.eieio.command_messages import SpinnakerRequestBuffers
+from spinnman.messages.eieio.command_messages import HostSendSequencedData
+from spinnman.messages.eieio.command_messages import SpinnakerRequestReadData
+from spinnman.messages.eieio.command_messages import HostDataRead
+from spinnman.messages.eieio.command_messages import EIEIOCommandHeader
+from spinnman.messages.eieio.command_messages import EIEIOCommandMessage
+from spinnman.messages.eieio.command_messages import DatabaseConfirmation
+from spinnman.constants import EIEIO_COMMAND_IDS
 
 
 def read_eieio_command_message(data, offset):
@@ -40,52 +29,35 @@ def read_eieio_command_message(data, offset):
     command_header = EIEIOCommandHeader.from_bytestring(data, offset)
     command_number = command_header.command
 
-    if (command_number ==
-            constants.EIEIO_COMMAND_IDS.DATABASE_CONFIRMATION.value):
+    if command_number == EIEIO_COMMAND_IDS.DATABASE_CONFIRMATION.value:
         return DatabaseConfirmation.from_bytestring(
             command_header, data, offset + 2)
-
     # Fill in buffer area with padding
-    elif (command_number ==
-            constants.EIEIO_COMMAND_IDS.EVENT_PADDING.value):
+    elif command_number == EIEIO_COMMAND_IDS.EVENT_PADDING.value:
         return PaddingRequest()
-
     # End of all buffers, stop execution
-    elif (command_number ==
-            constants.EIEIO_COMMAND_IDS.EVENT_STOP.value):
+    elif command_number == EIEIO_COMMAND_IDS.EVENT_STOP.value:
         return EventStopRequest()
-
     # Stop complaining that there is sdram free space for buffers
-    elif (command_number ==
-            constants.EIEIO_COMMAND_IDS.STOP_SENDING_REQUESTS.value):
+    elif command_number == EIEIO_COMMAND_IDS.STOP_SENDING_REQUESTS.value:
         return StopRequests()
-
     # Start complaining that there is sdram free space for buffers
-    elif (command_number ==
-            constants.EIEIO_COMMAND_IDS.START_SENDING_REQUESTS.value):
+    elif command_number == EIEIO_COMMAND_IDS.START_SENDING_REQUESTS.value:
         return StartRequests()
-
     # Spinnaker requesting new buffers for spike source population
-    elif (command_number ==
-            constants.EIEIO_COMMAND_IDS.SPINNAKER_REQUEST_BUFFERS.value):
+    elif command_number == EIEIO_COMMAND_IDS.SPINNAKER_REQUEST_BUFFERS.value:
         return SpinnakerRequestBuffers.from_bytestring(
             command_header, data, offset + 2)
-
     # Buffers being sent from host to SpiNNaker
-    elif (command_number ==
-            constants.EIEIO_COMMAND_IDS.HOST_SEND_SEQUENCED_DATA.value):
+    elif command_number == EIEIO_COMMAND_IDS.HOST_SEND_SEQUENCED_DATA.value:
         return HostSendSequencedData.from_bytestring(
             command_header, data, offset + 2)
-
     # Buffers available to be read from a buffered out vertex
-    elif (command_number ==
-            constants.EIEIO_COMMAND_IDS.SPINNAKER_REQUEST_READ_DATA.value):
+    elif command_number == EIEIO_COMMAND_IDS.SPINNAKER_REQUEST_READ_DATA.value:
         return SpinnakerRequestReadData.from_bytestring(
             command_header, data, offset + 2)
-
     # Host confirming data being read form SpiNNaker memory
-    elif (command_number ==
-            constants.EIEIO_COMMAND_IDS.HOST_DATA_READ.value):
+    elif command_number == EIEIO_COMMAND_IDS.HOST_DATA_READ.value:
         return HostDataRead.from_bytestring(
             command_header, data, offset + 2)
     return EIEIOCommandMessage(command_header, data, offset + 2)
