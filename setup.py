34c0db6dd47a7391f5e35472a57f747681ce91cd
Christian Brenninkmeijer <brenninc@cs.man.ac.uk>
2017-09-21 14:15:25 +0100

    Removed break and unified setup.py to all work the same

--- setup.py
+++ setup.py
@@ -11,7 +11,8 @@ assert __version__
 
 # Build a list of all project modules, as well as supplementary files
 main_package = "spinnman"
-data_extensions = {".aplx", ".xml", ".boot"}
+data_extensions = {".aplx", ".boot", ".json", ".xml", ".xsd"}
+config_extensions = {".cfg", ".template"}
 main_package_dir = os.path.join(os.path.dirname(__file__), main_package)
 start = len(main_package_dir)
 packages = []
@@ -21,14 +22,19 @@ for dirname, dirnames, filenames in os.walk(main_package_dir):
         package = "{}{}".format(
             main_package, dirname[start:].replace(os.sep, '.'))
         packages.append(package)
+    ext_set = set()
     for filename in filenames:
         _, ext = os.path.splitext(filename)
         if ext in data_extensions:
+            ext_set.add(ext)
+        if ext in config_extensions:
             package = "{}{}".format(
                 main_package, dirname[start:].replace(os.sep, '.'))
-            package_data[package].append("*{}".format(ext))
-
-            break
+            package_data[package].append(filename)
+    for ext in ext_set:
+        package = "{}{}".format(
+            main_package, dirname[start:].replace(os.sep, '.'))
+        package_data[package].append("*{}".format(ext))
 
 setup(
     name="SpiNNMan",
