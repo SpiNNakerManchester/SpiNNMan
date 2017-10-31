9dd09ebe1a8d6cf099f5dd7216826b170927242f
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2017-10-31 12:01:41 +0000

    Fixed tests

--- integration_tests/test_file_io.py
+++ integration_tests/test_file_io.py
@@ -1,55 +1,44 @@
-import unittest
 import numpy
 import os
 from spinnman.utilities.io.file_io import FileIO
 import struct
 
 
-class TestMemoryIO(unittest.TestCase):
+def test_file_io():
+    n_bytes = 1000
+    read_start_offset = 5
+    read_end_offset = -10
+    my_file = "test"
+    data = bytearray(numpy.random.randint(0, 256, n_bytes).astype("uint8"))
+    memory = FileIO(my_file, 0, n_bytes)
+    memory.write(bytes(data))
+    memory = memory[read_start_offset:read_end_offset]
+    read_data = bytearray(memory.read())
+    compare_data = data[read_start_offset:read_end_offset]
+    assert(compare_data == read_data)
 
-    def test_memory_io(self):
-        n_bytes = 1000
-        read_start_offset = 5
-        read_end_offset = -10
-        my_file = "test"
-        data = bytearray(numpy.random.randint(0, 256, n_bytes).astype("uint8"))
-        memory = FileIO(my_file, 0, n_bytes)
-        print "Written", memory.write(bytes(data)), "bytes"
-        memory = memory[read_start_offset:read_end_offset]
-        read_data = bytearray(memory.read())
-        compare_data = data[read_start_offset:read_end_offset]
-        print "Read", len(read_data), "bytes"
-        data_str = [hex(val) for val in compare_data]
-        read_str = [hex(val) for val in read_data]
-        self.assertEqual(
-            compare_data, read_data,
-            "Written data is not the same as read data:\n    {}\n    {}"
-            .format(data_str, read_str))
+    memory.seek(20)
+    test_data = bytearray(range(10))
+    memory.write(bytes(test_data))
+    memory.write(bytes(test_data))
+    memory.seek(-10, os.SEEK_CUR)
+    read_data = bytearray(memory.read(10))
+    assert(test_data == read_data)
 
-        memory.seek(20)
-        test_data = bytearray(range(10))
-        memory.write(bytes(test_data))
-        memory.write(bytes(test_data))
-        memory.seek(-10, os.SEEK_CUR)
-        read_data = bytearray(memory.read(10))
-        self.assertEqual(
-            test_data, read_data,
-            "Written data is not the same as read data:\n    {}\n    {}"
-            .format(
-                [hex(val) for val in test_data],
-                [hex(val) for val in read_data]))
+    _test_fill(memory, 100)
+    _test_fill(memory, 101)
+    _test_fill(memory, 102)
+    _test_fill(memory, 103)
 
-        memory.seek(124)
-        memory.write(struct.pack("B", 10))
-        memory.seek(112)
-        memory[112:124].fill(5)
-        memory.seek(112)
-        test_data = struct.unpack("<III", memory.read(12))
-        memory.close()
-        os.remove(my_file)
-        self.assertEqual(
-            test_data, (5, 5, 5),
-            "Filled data values unexpected: {}".format(test_data))
-        self.assertEqual(
-            struct.unpack("B", memory.read(1))[0], 10,
-            "Fill overwritten value")
+    memory.close()
+    os.remove(my_file)
+
+
+def _test_fill(memory, offset):
+    memory.seek(offset + 12)
+    memory.write(struct.pack("B", 10))
+    memory[offset:offset + 12].fill(5)
+    memory.seek(offset)
+    test_data = struct.unpack("<III", memory.read(12))
+    assert(test_data == (5, 5, 5))
+    assert(struct.unpack("B", memory.read(1))[0] == 10)
--- integration_tests/test_memory_io.py
+++ integration_tests/test_memory_io.py
@@ -1,65 +1,77 @@
-import unittest
-from spinnman.transceiver import create_transceiver_from_hostname
 from spinnman.utilities.io.memory_io import MemoryIO
 import numpy
 import os
 import struct
 
 
-class TestMemoryIO(unittest.TestCase):
+class _MockTransceiver(object):
 
-    def test_memory_io(self):
-        n_bytes = 1000
-        read_start_offset = 5
-        read_end_offset = -10
-        transceiver = create_transceiver_from_hostname(
-            "spinn-10.cs.man.ac.uk", 2)
-        transceiver.ensure_board_is_ready()
-        address = transceiver.malloc_sdram(0, 0, n_bytes, 30)
-        data = bytearray(numpy.random.randint(0, 256, n_bytes).astype("uint8"))
-        memory = MemoryIO(transceiver, 0, 0, address, address + n_bytes)
-        print "Written", memory.write(bytes(data)), "bytes"
-        read_memory = memory[read_start_offset:read_end_offset]
-        read_data = bytearray(read_memory.read())
-        compare_data = data[read_start_offset:read_end_offset]
-        print "Read", len(read_data), "bytes"
-        data_str = [hex(val) for val in compare_data]
-        read_str = [hex(val) for val in read_data]
-        self.assertEqual(
-            compare_data, read_data,
-            "Written data is not the same as read data:\n    {}\n    {}"
-            .format(data_str, read_str))
+    def __init__(self):
+        self._data = dict()
 
-        memory.seek(20)
-        test_data = bytearray(range(10))
-        memory.write(bytes(test_data))
-        memory.write(bytes(test_data))
-        memory.seek(-10, os.SEEK_CUR)
-        read_data = bytearray(memory.read(10))
-        self.assertEqual(
-            test_data, read_data,
-            "Written data is not the same as read data:\n    {}\n    {}"
-            .format(
-                [hex(val) for val in test_data],
-                [hex(val) for val in read_data]))
+    def _get_memory(self, x, y):
+        if (x, y) not in self._data:
+            self._data[(x, y)] = numpy.zeros(128 * 1024 * 1024, dtype="uint8")
+        return self._data[(x, y)]
 
-        self._test_fill(memory, 100)
-        self._test_fill(memory, 101)
-        self._test_fill(memory, 102)
-        self._test_fill(memory, 103)
+    def write_memory(self, x, y, address, data, n_bytes=None):
+        memory = self._get_memory(x, y)
+        if isinstance(data, int):
+            memory[address:address + 4] = numpy.array(
+                [data], dtype="uint32").view(dtype="uint8")
+        else:
+            if n_bytes is None:
+                n_bytes = len(data)
+            numpy_data = numpy.frombuffer(data[:n_bytes], dtype="uint8")
+            memory[address:address + n_bytes] = numpy_data
 
-        transceiver.stop_application(30)
-        transceiver.close()
+    def read_memory(self, x, y, address, n_bytes):
+        memory = self._get_memory(x, y)
+        return memory[address:address + n_bytes]
 
-    def _test_fill(self, memory, offset):
-        memory.seek(offset + 12)
-        memory.write(struct.pack("B", 10))
-        memory[offset:offset + 12].fill(5)
-        memory.seek(offset)
-        test_data = struct.unpack("<III", memory.read(12))
-        self.assertEqual(
-            test_data, (5, 5, 5),
-            "Filled data values unexpected: {}".format(test_data))
-        self.assertEqual(
-            struct.unpack("B", memory.read(1))[0], 10,
-            "Fill overwritten value")
+    def fill_memory(
+            self, x, y, address, repeat_value, bytes_to_fill, data_type):
+        memory = self._get_memory(x, y)
+        data_to_fill = numpy.array([repeat_value], dtype="uint{}".format(
+            data_type.value * 8)).view("uint8")
+        data_to_write = numpy.tile(
+            data_to_fill, bytes_to_fill / data_type.value)
+        memory[address:address + bytes_to_fill] = data_to_write
+
+
+def test_memory_io():
+    n_bytes = 1000
+    read_start_offset = 5
+    read_end_offset = -10
+    transceiver = _MockTransceiver()
+    address = 0
+    data = bytearray(numpy.random.randint(0, 256, n_bytes).astype("uint8"))
+    memory = MemoryIO(transceiver, 0, 0, address, address + n_bytes)
+    memory.write(bytes(data))
+    read_memory = memory[read_start_offset:read_end_offset]
+    read_data = bytearray(read_memory.read())
+    compare_data = data[read_start_offset:read_end_offset]
+    assert(compare_data == read_data)
+
+    memory.seek(20)
+    test_data = bytearray(range(10))
+    memory.write(bytes(test_data))
+    memory.write(bytes(test_data))
+    memory.seek(-10, os.SEEK_CUR)
+    read_data = bytearray(memory.read(10))
+    assert(test_data == read_data)
+
+    _test_fill(memory, 100)
+    _test_fill(memory, 101)
+    _test_fill(memory, 102)
+    _test_fill(memory, 103)
+
+
+def _test_fill(memory, offset):
+    memory.seek(offset + 12)
+    memory.write(struct.pack("B", 10))
+    memory[offset:offset + 12].fill(5)
+    memory.seek(offset)
+    test_data = struct.unpack("<III", memory.read(12))
+    assert(test_data == (5, 5, 5))
+    assert(struct.unpack("B", memory.read(1))[0] == 10)
