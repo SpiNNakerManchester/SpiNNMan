61ffc6c3f8c5948b4bf1a535977dd593aebd9d96
Donal Fellows <donal.k.fellows@manchester.ac.uk>
2017-12-11 10:09:07 +0000

    Tidy up the fill code more radically

--- spinnman/processes/fill_process.py
+++ spinnman/processes/fill_process.py
@@ -1,8 +1,13 @@
-import struct
 from enum import Enum
+import logging
+import struct
+
 from spinnman.messages.scp.impl import FillRequest, WriteMemory
 from spinnman.processes.abstract_multi_connection_process \
     import AbstractMultiConnectionProcess
+
+logger = logging.getLogger(__name__)
+ALIGNMENT = 4
 
 
 class FillDataType(Enum):
@@ -32,57 +37,65 @@ class FillProcess(AbstractMultiConnectionProcess):
     """ A process for filling memory
     """
 
+    PACKS = [struct.Struct("<{}B".format(i)) for i in range(ALIGNMENT)]
+
     # pylint: disable=too-many-arguments
 
     def __init__(self, connection_selector):
         AbstractMultiConnectionProcess.__init__(self, connection_selector)
 
-    def _write_pre_bytes(self, x, y, base_address, data_to_fill, size):
-        extra_bytes = (4 - (base_address % 4)) % 4
-        # Pre bytes is the first part of the data up to the first aligned
+    def _write_pre_bytes(self, x, y, base, data_to_fill, size):
+        extra_bytes = (ALIGNMENT - base % ALIGNMENT) % ALIGNMENT
+        if not extra_bytes:
+            return 0
+        # Pre_bytes is the first part of the data up to the first aligned
         # word
-        pre_bytes = struct.pack(
-            "<{}B".format(extra_bytes), *(data_to_fill[:extra_bytes]))
+        pre_bytes = self.PACKS[extra_bytes].pack(*data_to_fill[:extra_bytes])
         # Send the pre-data to make the memory aligned (or all the
         # data if the size is correct - note that pre_bytes[:size] will
         # return all of pre_bytes if size >= len(pre_bytes)
         pre_data = pre_bytes[:size]
         if not pre_data:
             return 0
-        self._send_request(WriteMemory(x, y, base_address, pre_data))
+        self._send_request(WriteMemory(x, y, base, pre_data))
         return extra_bytes
 
     def _write_fill(self, x, y, address, base, data_to_fill, size):
-        extra_bytes = (4 - (base % 4)) % 4
-        fill_bytes = size
-        if extra_bytes != 0:
-            fill_bytes -= 4
-        if not fill_bytes:
+        extra_bytes = (ALIGNMENT - base % ALIGNMENT) % ALIGNMENT
+        size = size - ALIGNMENT if extra_bytes else size
+        if not size:
             return 0
         # The data to send is the repeated fill data, from the end of the
-        # pre-data circling round to the start of the post-data
-        data_to_send = struct.unpack("<I", struct.pack("<4B", *([
-            data_to_fill[i % 4]
-            for i in range(extra_bytes, extra_bytes + 4)])))[0]
-        self._send_request(FillRequest(
-            x, y, address, data_to_send, fill_bytes))
-        return fill_bytes
+        # pre-data circling round to the start of the post-data; we double
+        # it up so that we don't need to use mod (it's pretty small).
+        data = data_to_fill + data_to_fill
+        fill_word = struct.unpack(
+            "<I", data[extra_bytes:extra_bytes + ALIGNMENT - 1])[0]
+        self._send_request(FillRequest(x, y, address, fill_word, size))
+        return size
 
     def _write_post_bytes(
             self, x, y, address, base, data_to_fill, bytes_to_write):
-        extra_bytes = (4 - (base % 4)) % 4
         # Post bytes is the last part of the data from the end of the last
-        # aligned word
-        n_post_bytes = (4 - extra_bytes) % 4
-
-        if not n_post_bytes or not bytes_to_write:
+        # aligned word; the number of bytes to write here is exactly the
+        # number of bytes later than a word boundary the initial address is.  
+        n_bytes = base % ALIGNMENT
+        if not n_bytes or not bytes_to_write:
             return
-        post_bytes = struct.pack(
-            "<{}B".format(n_post_bytes), *(data_to_fill[-n_post_bytes:]))
+
+        post_bytes = self.PACKS[n_bytes].pack(*data_to_fill[-n_bytes:])
         self._send_request(WriteMemory(
             x, y, address, post_bytes[:bytes_to_write]))
 
     def fill_memory(self, x, y, base_address, data, size, data_type):
+        """
+        :type x: int
+        :type y: int
+        :type base_address: int
+        :type data: int
+        :type size: int
+        :type data_type: spinnman.processes.fill_process.FillDataType
+        """
         # Don't do anything if there is nothing to do!
         if size == 0:
             return
@@ -92,32 +105,36 @@ class FillProcess(AbstractMultiConnectionProcess):
             raise Exception(
                 "The size of {} bytes to fill is not divisible by the size of"
                 " the data of {} bytes".format(size, data_type.value))
+        if base_address % ALIGNMENT:
+            logger.warn(
+                "Unaligned fill starting at %d; please use aligned fills",
+                base_address)
 
         # Get a word of data regardless of the type
         data_to_fill = bytearray(struct.pack(
             str(data_type.struct_type),
-            *([data] * (4 / data_type.value))))
+            *([data] * (ALIGNMENT / data_type.value))))
 
-        bytes_to_write = size
+        written = 0
         address = base_address
 
         # Send the pre-data to make the memory aligned (or all the
         # data if the size is correct - note that pre_bytes[:size] will
         # return all of pre_bytes if size >= len(pre_bytes)
         delta = self._write_pre_bytes(x, y, base_address, data_to_fill, size)
-        bytes_to_write -= delta
+        written += delta
         address += delta
 
         # Fill as much as possible
         delta = self._write_fill(
             x, y, address, base_address, data_to_fill, size)
-        bytes_to_write -= delta
+        written += delta
         address += delta
 
         # Post bytes is the last part of the data from the end of the last
         # aligned word; send them if required
         self._write_post_bytes(
-            x, y, address, base_address, data_to_fill, bytes_to_write)
+            x, y, address, base_address, data_to_fill, size - written)
 
         # Wait for all the packets to be confirmed and then check there
         # are no errors
