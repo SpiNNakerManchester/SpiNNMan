da40357b63aa4f4e232cc68064d954065e8cf628
Donal Fellows <donal.k.fellows@manchester.ac.uk>
2018-01-31 11:29:53 +0000

    Flake8 and small refactor

--- spinnman/utilities/reports.py
+++ spinnman/utilities/reports.py
@@ -1,4 +1,4 @@
-import os
+import os.path
 import time
 import logging
 from spinn_utilities.log import FormatAdapter
@@ -20,39 +20,40 @@ def generate_machine_report(report_directory, machine, connections):
     :rtype: None
     :raise IOError: when a file cannot be opened for some reason
     """
-    file_name = report_directory + os.sep + "machine_structure.rpt"
+    file_name = os.path.join(report_directory, "machine_structure.rpt")
     time_date_string = time.strftime("%c")
     try:
         with open(file_name, "w") as f:
-            f.write("\t\tTarget SpiNNaker Machine Structure\n")
-            f.write("\t\t==================================\n\n")
-            f.write("Generated: %s" % time_date_string)
-            f.write(" for target machine '{}'".format(connections))
-            f.write("\n\n")
-
-            x_dim = machine.max_chip_x + 1
-            y_dim = machine.max_chip_y + 1
-            f.write("Machine dimensions (in chips) x : {}  y : {}\n\n".format(
-                x_dim, y_dim))
-
+            _write_header(f, time_date_string, machine, connections)
             # TODO: Add further details on the target machine.
-            f.write("\t\tMachine router information\n")
-            f.write("\t\t==========================\n\n")
             for x in range(machine.max_chip_x + 1):
                 for y in range(machine.max_chip_y + 1):
                     _write_chip_router_report(f, machine, x, y)
     except IOError:
-        logger.error("Generate_placement_reports: Can't open file {} for "
-                     "writing.", file_name)
+        logger.error(
+            "Generate_placement_reports: Can't open file {} for writing.",
+            file_name)
         raise
+
+
+def _write_header(f, timestamp, machine, connections):
+    f.write("\t\tTarget SpiNNaker Machine Structure\n")
+    f.write("\t\t==================================\n")
+    f.write("\nGenerated: {} for target machine '{}'\n\n".format(
+        timestamp, connections))
+    f.write("Machine dimensions (in chips) x : {}  y : {}\n\n".format(
+        machine.max_chip_x + 1, machine.max_chip_y + 1))
+    f.write("\t\tMachine router information\n")
+    f.write("\t\t==========================\n")
+
 
 def _write_chip_router_report(f, machine, x, y):
     chip = machine.get_chip_at(x, y)
     if chip:
-        f.write("Information for chip {}:{}\n".format(chip.x, chip.y))
+        f.write("\nInformation for chip {}:{}\n".format(chip.x, chip.y))
         f.write("Neighbouring chips \n{}\n".format(
             chip.router.get_neighbouring_chips_coords()))
         f.write("Router list of links for this chip are: \n")
         for link in chip.router.links:
             f.write("\t{}\n".format(link))
-        f.write("\t\t==========================\n\n")
+        f.write("\t\t==========================\n")
