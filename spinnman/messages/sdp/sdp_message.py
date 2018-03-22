217c7fe69bbef24abd3873060c3e9a8b6bf62228
alan-stokes <alan.barry.stokes@gmail.com>
2018-03-22 16:13:53 +0000

    doc string issue

--- spinnman/messages/sdp/sdp_message.py
+++ spinnman/messages/sdp/sdp_message.py
@@ -15,7 +15,7 @@ class SDPMessage(object):
         :type sdp_header:\
             :py:class:`spinnman.messages.sdp.sdp_header.SDPHeader`
         :param data: The data of the SDP packet, or None if no data
-        :type data: str
+        :type data: str or None
         :param offset: The offset where the valid data starts
         :type offset: int
         :raise None: No known exceptions are thrown
