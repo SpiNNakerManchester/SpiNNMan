b80763433ba4d9f6cc38eac90327b6eff933302b
Andrew Gait <andrew.gait@manchester.ac.uk>
2018-06-12 11:11:59 +0100

    flake8 python3 prints

--- get_cores_in_run_state.py
+++ get_cores_in_run_state.py
@@ -25,12 +25,12 @@ try:
     count_finished = transceiver.get_core_state_count(app_id,
                                                       CPUState.FINISHED)
     count_run = transceiver.get_core_state_count(app_id, CPUState.RUNNING)
-    print ('running: ', count_run, ' finished: ', count_finished)
+    print('running: ', count_run, ' finished: ', count_finished)
 
     machine = transceiver.get_machine_details()
-    print ('machine max x: ', machine.max_chip_x,
-           ' max y: ', machine.max_chip_y)
-    print ('machine chips: ', machine.chips)
+    print('machine max x: ', machine.max_chip_x,
+          ' max y: ', machine.max_chip_y)
+    print('machine chips: ', machine.chips)
 
     cores_finished = ()
     all_core_subsets = []
@@ -50,15 +50,15 @@ try:
     for core in cores_running:
         for pid in core.processor_ids:
             if (pid != 0) and (pid != 16):
-                print 'run core: ', core.x, ' ', core.y, ' ', pid
+                print('run core: ', core.x, ' ', core.y, ' ', pid)
 
     for core in cores_finished:
         for pid in core.processor_ids:
-            print 'finished core: ', core.x, ' ', core.y, ' ', pid
+            print('finished core: ', core.x, ' ', core.y, ' ', pid)
 
     for core in cores_watchdog:
         for pid in core.processor_ids:
-            print 'watchdog core: ', core.x, ' ', core.y, ' ', pid
+            print('watchdog core: ', core.x, ' ', core.y, ' ', pid)
 
 finally:
     transceiver.close()
