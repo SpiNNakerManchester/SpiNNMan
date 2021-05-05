2f0bb147befd1ed1341878e4d29df59f35879532
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2021-05-05 11:37:02 +0100

    cfg and config checker

--- unittests/test_cfg_checker.py
+++ unittests/test_cfg_checker.py
@@ -13,10 +13,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import os
 import unittest
-from spinn_utilities.config_holder import (
-    check_python_file, find_double_defaults)
+from spinn_utilities.config_holder import run_config_checks
 from spinnman.config_setup import reset_configs
 
 
@@ -26,17 +24,5 @@ class TestCfgChecker(unittest.TestCase):
     def setUpClass(cls):
         reset_configs()
 
-    def test_cfg_checker(self):
-        module = __import__("spinnman")
-        path = module.__file__
-        directory = os.path.dirname(path)
-        for root, dirs, files in os.walk(directory):
-            for file_name in files:
-                if file_name.endswith(".py"):
-                    if file_name == "config_holder.py":
-                        continue
-                    py_path = os.path.join(root, file_name)
-                    check_python_file(py_path)
-
-    def test_double_defaults(self):
-        find_double_defaults()
+    def test_config_checks(self):
+        run_config_checks("spinnman", exceptions="test.cfg")
