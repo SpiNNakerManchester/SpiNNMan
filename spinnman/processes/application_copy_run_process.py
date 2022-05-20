571dda95ee3c16b9569a16919c57ac6987ddddd3
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2022-05-20 16:17:08 +0100

    Docs

--- spinnman/processes/application_copy_run_process.py
+++ spinnman/processes/application_copy_run_process.py
@@ -18,6 +18,17 @@ from .abstract_multi_connection_process import AbstractMultiConnectionProcess
 
 
 def _get_next_chips(old_next_chips, machine, chips_done):
+    """ Get the chips that are adjacent to the last set of chips, which
+        haven't yet been loaded.  Also returned are the links for each chip,
+        which gives the link which should be read from to get the data.
+
+    :param list(int,Chip) old_next_chips:
+        The chips to find the chips adjacent to
+    :param Machine machine: The machine containing the chips
+    :param set(int,int) The coordinates of chips that have already been done
+    :return: A list of (link to read from, chip to be copied to)
+    :rtype: list(int,Chip)
+    """
     next_chips = set()
     for _old_link, chip in old_next_chips:
         for link in chip.router.links:
@@ -31,9 +42,29 @@ def _get_next_chips(old_next_chips, machine, chips_done):
 
 
 class ApplicationCopyRunProcess(AbstractMultiConnectionProcess):
+    """ Process to start a binary on a subset of cores on a subset of chips
+        of a machine, performed by, on each chip, copying the data from
+        an adjacent chip and then starting the binary.  This goes to each
+        chip in turn, and so detects failures early on, as well as ensuring
+        that the copy and execution is done in the case of success i.e. this
+        ensures that if all commands are successful, the full binary has been
+        copied and started.
+
+        NOTE: The binary must have been loaded to the boot chip before this is
+        called!
+    """
     __slots__ = []
 
     def run(self, machine, size, app_id, core_subsets, wait):
+        """ Run the process.
+
+        :param Machine machine: The machine to run the process on
+        :param int size: The size of the binary to copy
+        :param int app_id: The application id to assign to the running binary
+        :param CoreSubsets core_subsets: The cores to load the binary on to
+        :param bool wait:
+            Whether to put the binary in "wait" mode or run it straight away
+        """
         boot_chip = machine.boot_chip
         chips_done = set((boot_chip.x, boot_chip.y))
         next_chips = _get_next_chips([(None, boot_chip)], machine, chips_done)
