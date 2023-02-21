7c153165568ce8c7b3d4c8eb180aa59f4e435a68
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2023-02-21 12:33:25 +0000

    switch to excluded_because_in_init style

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -414,6 +414,4 @@ for fl in os.listdir("."):
 os.chdir("../..")  # WARNING! RELATIVE FILENAMES CHANGE MEANING HERE!
 apidoc.main([
     '-o', _output_dir, _package_base,
-    "spinnman/messages/spinnaker_boot/boot_data/*",
-    # Special case: Don't want that empty package at all.
     *excluded_because_in_init(_package_base)])
