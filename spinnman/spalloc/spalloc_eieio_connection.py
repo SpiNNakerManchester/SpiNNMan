127412a1db054a5cb908521e7c605b2eab94299f
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2023-03-17 16:02:35 +0000

    Allow non-reception of response (to be received elsewhere)

--- spinnman/spalloc/spalloc_eieio_connection.py
+++ spinnman/spalloc/spalloc_eieio_connection.py
@@ -82,12 +82,14 @@ class SpallocEIEIOConnection(
         :rtype: tuple(int,int)
         """
 
-    def update_tag(self, tag: int):
+    def update_tag(self, tag: int, do_receive: bool = True):
         """
         Update the given tag on the connected ethernet chip to send messages to
         this connection.
 
         :param int tag: The tag ID to update
+        :param bool do_receive:
+            Whether to do the reception of the response or not
         :raises SpinnmanTimeoutException:
             If the message isn't handled within a reasonable timeout.
         :raises SpinnmanUnexpectedResponseCodeException:
@@ -102,9 +104,10 @@ class SpallocEIEIOConnection(
         for _try in range(_NUM_UPDATE_TAG_TRIES):
             try:
                 self.send(data)
-                response_data = self.receive(_UPDATE_TAG_TIMEOUT)
-                request.get_scp_response().read_bytestring(
-                    response_data, len(_TWO_SKIP))
+                if do_receive:
+                    response_data = self.receive(_UPDATE_TAG_TIMEOUT)
+                    request.get_scp_response().read_bytestring(
+                        response_data, len(_TWO_SKIP))
                 return
             except SpinnmanTimeoutException as e:
                 if _try + 1 == _NUM_UPDATE_TAG_TRIES:
