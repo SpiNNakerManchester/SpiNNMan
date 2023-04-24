d53eff4c00221268a7f3a277377e1d34c2a35b93
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2023-04-24 10:21:25 +0100

    only start date on copyright

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -79,7 +79,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'SpiNNMan'
-copyright = u'2014-2021'
+copyright = u'2014'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -299,7 +299,7 @@ texinfo_documents = [
 epub_title = u'SpiNNMan'
 epub_author = u''
 epub_publisher = u''
-epub_copyright = u'2014-2017'
+epub_copyright = u'2014'
 
 # The basename for the epub file. It defaults to the project name.
 # epub_basename = u'data_allocation'
