1b60f228e90622b8c61a7104625a795df40b97fb
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2023-08-04 16:53:08 +0100

    fix sillyness

--- spinnman/transceiver/base_transceiver.py
+++ spinnman/transceiver/base_transceiver.py
@@ -301,7 +301,7 @@ class BaseTransceiver(ExtendableTransceiver, metaclass=AbstractBase):
     @overrides(AbstractTransceiver.send_sdp_message)
     def send_sdp_message(self, message, connection=None):
         if connection is None:
-            sconnection_to_use = self._scamp_connections[random.randint(
+            connection_to_use = self._scamp_connections[random.randint(
                 0, len(self._scamp_connections) - 1)]
         else:
             connection_to_use = connection
