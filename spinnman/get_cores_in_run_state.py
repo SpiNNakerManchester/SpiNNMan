26121d2fe0c270908e597204a67dd4df80526f77
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2023-08-14 16:59:55 +0100

    bmp_names is None so dont use

--- spinnman/get_cores_in_run_state.py
+++ spinnman/get_cores_in_run_state.py
@@ -83,7 +83,6 @@ def _make_transceiver(host, version, bmp_names):
         config = BoardTestConfiguration()
         config.set_up_remote_board()
         host = config.remotehost
-        bmp_names = config.bmp_names
         auto_detect_bmp = config.auto_detect_bmp
     else:
         if version is None:
@@ -96,8 +95,7 @@ def _make_transceiver(host, version, bmp_names):
 
     print(f"talking to SpiNNaker system at {host}")
     return create_transceiver_from_hostname(
-        host, bmp_connection_data=bmp_names,
-        auto_detect_bmp=auto_detect_bmp)
+        host, auto_detect_bmp=auto_detect_bmp)
 
 
 def main(args):
