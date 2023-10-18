b60d9f6828834bd17bc7eeffc80754ed4e6b4de9
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2023-10-18 08:49:01 +0100

    define _progress in init

--- spinnman/processes/get_machine_process.py
+++ spinnman/processes/get_machine_process.py
@@ -93,6 +93,7 @@ class GetMachineProcess(AbstractMultiConnectionProcess):
         # Maps between virtual and physical cores
         self._virtual_to_physical_map: Dict[XY, bytes] = dict()
         self._physical_to_virtual_map: Dict[XY, bytes] = dict()
+        self._progress: ProgressBar = None
 
     def _make_chip(self, chip_info: ChipSummaryInfo, machine: Machine) -> Chip:
         """
