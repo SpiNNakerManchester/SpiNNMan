0ec8eda46d6644e419ec69a3a55ebbabe3597411
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2023-10-19 07:48:46 +0100

    flake8

--- unittests/model_tests/test_cpu_infos.py
+++ unittests/model_tests/test_cpu_infos.py
@@ -77,5 +77,6 @@ class TestCpuInfos(unittest.TestCase):
         self.assertSetEqual(set(infos1),
                             {(0, 0, 1), (0, 0, 2), (1, 0, 1), (1, 0, 2)})
 
+
 if __name__ == '__main__':
     unittest.main()
