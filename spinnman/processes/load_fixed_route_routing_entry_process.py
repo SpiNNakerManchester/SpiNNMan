09df1d805aa25214d71e8400ea7331d5ff1ad784
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2024-03-14 12:16:33 +0000

    FixedRouteEntry extends BaseMulticastRoutingEntry

--- spinnman/processes/load_fixed_route_routing_entry_process.py
+++ spinnman/processes/load_fixed_route_routing_entry_process.py
@@ -36,7 +36,6 @@ class LoadFixedRouteRoutingEntryProcess(AbstractMultiConnectionProcess):
         :param int app_id: The ID of the application with which to associate
             the routes.  If not specified, defaults to 0.
         """
-        route_entry = Router.convert_routing_table_entry_to_spinnaker_route(
-            fixed_route)
         with self._collect_responses():
-            self._send_request(FixedRouteInit(x, y, route_entry, app_id))
+            self._send_request(FixedRouteInit(
+                x, y, fixed_route.spinnaker_route, app_id))
