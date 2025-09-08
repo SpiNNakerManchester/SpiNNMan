2d6c5c8cdb5c4d070d6f1fdf400a07ba51471608
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2025-09-08 10:46:56 +0100

    move machine_generator and ViewData to SpiNNMan

--- spinnman/data/spinnman_data_writer.py
+++ spinnman/data/spinnman_data_writer.py
@@ -13,9 +13,12 @@
 # limitations under the License.
 
 import logging
+from typing import Optional
 from spinn_utilities.log import FormatAdapter
 from spinn_utilities.overrides import overrides
 from spinn_machine.data.machine_data_writer import MachineDataWriter
+from spinnman.spalloc import MachineAllocationController
+from spinnman.spalloc.spalloc_allocator import SpallocJobController
 from spinnman.transceiver import Transceiver
 from .spinnman_data_view import _SpiNNManDataModel, SpiNNManDataView
 
@@ -93,6 +96,27 @@ class SpiNNManDataWriter(MachineDataWriter, SpiNNManDataView):
     def _soft_reset(self) -> None:
         MachineDataWriter._soft_reset(self)
         self._local_soft_reset()
+
+    def set_allocation_controller(self, allocation_controller: Optional[
+            MachineAllocationController]) -> None:
+        """
+        Sets the allocation controller variable.
+
+        :param allocation_controller:
+        """
+        if allocation_controller and not isinstance(
+                allocation_controller, MachineAllocationController):
+            raise TypeError(
+                "allocation_controller must be a MachineAllocationController")
+        self.__data._spalloc_job = None
+        self.__data._allocation_controller = allocation_controller
+        if allocation_controller is None:
+            return
+        if allocation_controller.proxying:
+            if not isinstance(allocation_controller, SpallocJobController):
+                raise NotImplementedError(
+                    "Expecting only the SpallocJobController to be proxying")
+            self.__data._spalloc_job = allocation_controller.job
 
     def set_transceiver(self, transceiver: Transceiver) -> None:
         """
