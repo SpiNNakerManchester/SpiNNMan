a6bcf9b296d72a6f693dfcfd9d5a17ef0d530b4f
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2025-12-08 15:10:25 +0000

    Use our own queue so we can get some control on it

--- spinnman/connections/connection_listener.py
+++ spinnman/connections/connection_listener.py
@@ -14,7 +14,8 @@
 from __future__ import annotations
 import logging
 from threading import Thread
-from typing import Callable, Generic, List, TypeVar
+from queue import Queue
+from typing import Callable, Generic, List, TypeVar, Tuple
 from concurrent.futures import ThreadPoolExecutor, Future
 from spinn_utilities.log import FormatAdapter
 from spinnman.exceptions import SpinnmanEOFException
@@ -24,6 +25,7 @@ from spinnman.connections.abstract_classes import Listenable
 T = TypeVar("T")
 logger = FormatAdapter(logging.getLogger(__name__))
 _POOL_SIZE = 4
+_MAX_QUEUE_SIZE = 100
 _TIMEOUT = 1
 
 
@@ -33,14 +35,17 @@ class ConnectionListener(Thread, Generic[T]):
     messages when they arrive.
     """
     __slots__ = (
-        "__callback_pool",
+        "__process_threads",
+        "__process_queue",
         "__callbacks",
         "__connection",
         "__done",
-        "__timeout")
+        "__timeout",
+        "__drop_count")
 
     def __init__(self, connection: Listenable[T],
-                 n_processes: int = _POOL_SIZE, timeout: float = _TIMEOUT):
+                 n_processes: int = _POOL_SIZE, timeout: float = _TIMEOUT,
+                 max_queue_size: int = _MAX_QUEUE_SIZE) -> None:
         """
         :param connection: A connection to listen to
         :param n_processes:
@@ -48,15 +53,23 @@ class ConnectionListener(Thread, Generic[T]):
         :param timeout:
             How long to wait for messages before checking to see if the
             connection is to be terminated.
+        :param max_queue_size:
+            The maximum size of the queue of messages to be processed.
         """
         super().__init__(
             name=f"Connection listener for connection {connection}")
         self.daemon = True
         self.__connection = connection
         self.__timeout = timeout
-        self.__callback_pool = ThreadPoolExecutor(max_workers=n_processes)
         self.__done = False
         self.__callbacks: List[Callable[[T], None]] = []
+        self.__process_queue: Queue[Tuple[Callable, T]] = Queue(
+            maxsize=max_queue_size)
+        self.__process_threads: List[_ProcessThread[T]] = [
+            _ProcessThread(self.__process_queue)
+            for _ in range(n_processes)
+        ]
+        self.__drop_count: int = 0
 
     def __run_step(self, handler: Callable[[], T]) -> None:
         """
@@ -65,31 +78,35 @@ class ConnectionListener(Thread, Generic[T]):
         if self.__connection.is_ready_to_receive(timeout=self.__timeout):
             message = handler()
             for callback in self.__callbacks:
-                future = self.__callback_pool.submit(
-                    callback, message)
-                future.add_done_callback(self.__done_callback)
-
-    def __done_callback(self, future: Future[None]) -> None:
-        try:
-            future.result()
-        except Exception:  # pylint: disable=broad-except
-            logger.exception("problem in listener call")
+                if self.__process_queue.full():
+                    self.__drop_count += 1
+                else:
+                    self.__process_queue.put((callback, message))
 
     def run(self) -> None:
         """
         Implements the listening thread.
         """
-        with self.__callback_pool:
-            handler = self.__connection.get_receive_method()
-            while not self.__done:
-                try:
-                    self.__run_step(handler)
-                except SpinnmanEOFException:
-                    self.__done = True
-                except Exception:  # pylint: disable=broad-except
-                    if not self.__done:
-                        logger.warning("problem when dispatching message",
-                                       exc_info=True)
+        self.__drop_count = 0
+        for process_thread in self.__process_threads:
+            process_thread.start()
+        handler = self.__connection.get_receive_method()
+        while not self.__done:
+            try:
+                self.__run_step(handler)
+            except SpinnmanEOFException:
+                self.__done = True
+            except Exception:  # pylint: disable=broad-except
+                if not self.__done:
+                    logger.warning("problem when dispatching message",
+                                    exc_info=True)
+        for process_thread in self.__process_threads:
+            process_thread.stop()
+            process_thread.join()
+        if self.__drop_count > 0:
+            logger.warning(
+                "{} messages were dropped due to full processing queue",
+                self.__drop_count)
 
     def add_callback(self, callback: Callable[[T], None]) -> None:
         """
@@ -112,3 +129,33 @@ class ConnectionListener(Thread, Generic[T]):
         """
         self.__done = True
         self.join()
+
+class _ProcessThread(Thread, Generic[T]):
+    """
+    A thread that processes messages from a queue.
+    """
+    __slots__ = ("__process_queue")
+
+    def __init__(self, process_queue: Queue[Tuple[Callable, T]]) -> None:
+        """
+        :param process_queue:
+            The queue to get things to do from
+        """
+        super().__init__(daemon=True)
+        self.__process_queue = process_queue
+
+    def run(self) -> None:
+        """
+        The thread's main loop.
+        """
+        while True:
+            callback, message = self.__process_queue.get()
+            if message is None:
+                break
+            callback(message)
+
+    def stop(self) -> None:
+        """
+        Stop the thread.
+        """
+        self.__process_queue.put((lambda x: None, None))
